c63ac320143d8a4c00e3ac85429ff3dabec0c85c
fxxggah <00001106883950sp@gmail.com>
2026-07-02 15:09:11 -0300

    fix: altera a main para o deploy

--- src/main/java/com/katallo/KatalloPlatformApplication.java
+++ src/main/java/com/katallo/KatalloPlatformApplication.java
@@ -8,22 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KatalloPlatformApplication {
 
 	public static void main(String[] args) {
-
-		Dotenv dotenv = Dotenv.load();
-
-		System.setProperty("CLOUDINARY_CLOUD_NAME", dotenv.get("CLOUDINARY_CLOUD_NAME"));
-		System.setProperty("CLOUDINARY_API_KEY", dotenv.get("CLOUDINARY_API_KEY"));
-		System.setProperty("CLOUDINARY_API_SECRET", dotenv.get("CLOUDINARY_API_SECRET"));
-
-		System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
-		System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
-		System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
-		System.setProperty("DB_USER", dotenv.get("DB_USER"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-
-		System.setProperty("MY_CLIENT_ID", dotenv.get("MY_CLIENT_ID"));
-		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
-
 		SpringApplication.run(KatalloPlatformApplication.class, args);
 	}
 }
